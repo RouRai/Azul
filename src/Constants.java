2ce7397ca4144825b47a873e5143f92aaf79a116
SLHSCS311 <SLHSCS311@KATYISD.ORG>
2022-10-13 12:11:01 -0500

    Initial push

---
+++ src/Constants.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+
+public class Constants {
+    private final static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    public static final int WIDTH = (int) screenSize.getWidth();
+    public static final int HEIGHT = (int) screenSize.getHeight();
+}
