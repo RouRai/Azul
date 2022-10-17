26dab621dbb3b7ace461b1898cfff3bc9c57f2c7
SLHSCS311 <SLHSCS311@KATYISD.ORG>
2022-10-17 11:53:13 -0500

    initial commit

---
+++ src/Azul.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Azul extends JFrame{
+
+    public Azul(String name){
+        super(name);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(Constants.WIDTH, Constants.HEIGHT);
+        add(new StartScreen());
+        setVisible(true);
+    }
+}
---
+++ src/StartScreen.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+
+public class StartScreen extends JPanel {
+    JButton startBut;
+    ImageIcon startIcon;
+    BufferedImage img;
+}
