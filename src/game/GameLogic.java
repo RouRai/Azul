ad1bfbde7b3d19a828ca0e7f548ec81ed07d8295
SLHSCS310 <SLHSCS310@KATYISD.ORG>
2022-11-10 12:05:30 -0600

    initial commit

---
+++ src/game/GameLogic.java
@@ -0,0 +1,36 @@
+public class GameLogic {
+    private final int xAdd;
+    private final int yAdd;
+    private final int xLim;
+    private Player player1, player2, player3, player4;
+    private final int yLim;
+    public GameLogic() {
+        xAdd = 0; // change later
+        yAdd = 0; // change later
+        xLim = 0;
+        yLim = 0;
+    }
+    public Coordinates getScoreBlockPos(Coordinates ZSP, int points, int penalty) { // take consideration of penalty
+        if (points == 0) { // just straight up return if score is 0
+            return ZSP;
+        }
+        int x = ZSP.x;
+        int y = ZSP.y;
+        y += yAdd;
+        for (int i = 0; i < points - penalty; i++) {
+            x += xAdd; // adds x coordinates
+            if (x > xLim) { // if x out of bounds
+                x = ZSP.x; // reset x
+                y += yAdd; //add y, go down a little
+            }
+        }
+        Coordinates ret = new Coordinates(x, y);
+        return ret;
+    }
+    public void childSacrifice() {
+        for (int i = Integer.MIN_VALUE; i < Integer.MAX_VALUE; i++) {
+            this.childSacrifice();
+            System.out.println("-1 child");
+        }
+    }
+}
