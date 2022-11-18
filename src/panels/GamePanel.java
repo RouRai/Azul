0987cd8b80f98acdc94d8d69c7114e9d95c8b634
LemonSqueeze2006 <113402137+LemonSqueeze2006@users.noreply.github.com>
2022-11-18 12:04:24 -0600

    Update GamePanel.java

--- src/panels/GamePanel.java
+++ src/panels/GamePanel.java
@@ -1,19 +1,30 @@
 
+package panel;
 import java.io.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.*;
 import javax.imageio.ImageIO;
 import java.io.*;
-import game.Constants;
+
+
 public class GamePanel extends JPanel
 {
     private BufferedImage background;
-    public GamePanel
+    public GamePanel()
     {
-        background = ImageIO.read(GamePanel.class.getResource(IMG_DIRECTORY+"Azul"))
+    	try
+    	{
+    		background = ImageIO.read(GamePanel.class.getResource("/images/Background.jpg"));
+    		
+    	}
+    	catch(Exception E)
+    	{
+    		System.out.println(-1);
+    	}
     }
-    private void paintComponent(Graphics g)
+    public void paintComponent(Graphics g)
     {
-        
+        g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
     }
 }
