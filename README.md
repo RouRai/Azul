1f28a787226131c3a010d1ac0fde4267af4a5c7a
Rounak Rai <rouna@Rounaks-MBP.ln.ml-ai.us>
2022-11-20 14:35:17 -0600

    added data structures, untested

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-# ActualGame
-no
-=======
-# Azul
 This is our CS3 Azul Game Project!
->>>>>>> startScreen
---
+++ src/panels/StartScreenPanel.java
@@ -0,0 +1,13 @@
+package panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StartScreenPanel extends JPanel{
+
+    
+    public StartScreenPanel() {
+
+    }
+    
+}
