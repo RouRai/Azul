d31f7875de917b6162740a8edd9b92c5a854cda7
Rounak Rai <rouna@Rounaks-MBP.ln.ml-ai.us>
2022-11-21 22:01:17 -0600

    changed layout of buttons in panels and frame

--- src/panels/StartScreenPanel.java
+++ src/panels/StartScreenPanel.java
@@ -5,13 +5,20 @@ import javax.swing.*;
 import game.Constants;
 import java.awt.image.BufferedImage;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
 
 public class StartScreenPanel extends JPanel{
 
     private BufferedImage background;
+    private JButton ruleButton;
+    private CardLayout cl;
 
-    public StartScreenPanel() {
+    public StartScreenPanel(CardLayout cl) {
+        this.cl = cl;
         background = Constants.getImage("AzulStartScreenBackground");
+        setUpButtons();
     }
 
     public void paintComponent(Graphics g) {
@@ -20,6 +27,19 @@ public class StartScreenPanel extends JPanel{
         // Draw background
         g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
 
+    }
+
+    private void setUpButtons() {
+        ruleButton = new JButton("Rules");
+
+        ruleButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cl.show(Constants.PANEL_CONT, Constants.RULE_PANEL);
+            }
+        });
+
+        add(ruleButton, BorderLayout.LINE_END);
     }
     
 }   
--- src/panels/TestFrame.java
+++ src/panels/TestFrame.java
@@ -26,17 +26,13 @@ public class TestFrame extends JFrame{
     }
 
     private void setUpPanels() {
-        setUpButtons();
 
         // Add panels to HashMap
         panels.put(Constants.RULE_PANEL, new RulebookPanel(cl));
-        panels.put(Constants.START_PANEL, new StartScreenPanel());
+        panels.put(Constants.START_PANEL, new StartScreenPanel(cl));
 
         // Set up layout 
         Constants.PANEL_CONT.setLayout(cl);
-
-        // Set up buttons in the panels
-        panels.get(Constants.START_PANEL).add(ruleButton, BorderLayout.LINE_END);
 
         // Add panels to panelCont
         Constants.PANEL_CONT.add(panels.get(Constants.START_PANEL), Constants.START_PANEL);
@@ -45,17 +41,6 @@ public class TestFrame extends JFrame{
         // Show startScreen at beginning
         cl.show(Constants.PANEL_CONT, Constants.START_PANEL);
 
-    }
-
-    private void setUpButtons() {
-        ruleButton = new JButton("Rules");
-
-        ruleButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                cl.show(Constants.PANEL_CONT, Constants.RULE_PANEL);
-            }
-        });
     }
 
 
