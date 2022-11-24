7ccf5812e4dee7c4ed679d3935c84353b3935d69
NotHuayra <sam.qiao07@gmail.com>
2022-11-23 20:36:12 -0600

    removed useless spaces sriharsha fix the test fram and start screen panel

--- src/game/Constants.java
+++ src/game/Constants.java
@@ -1,17 +1,13 @@
 package game;
-
 import java.awt.*;
 import java.awt.image.BufferedImage;
-
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
-
 public class Constants {
     // Screen dimensions
     private final static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     public static final int WIDTH = (int) screenSize.getWidth();
     public static final int HEIGHT = (int) screenSize.getHeight();
-
     // Panel Constants
     public static final JPanel PANEL_CONT = new JPanel();
     
@@ -26,7 +22,6 @@ public class Constants {
     public static final int RED_TILE_ID = 4;
     public static final int WHITE_TILE_ID = 5;
     public static final int YELLOW_TILE_ID = 6;
-
     // Tile String Constants
     public static final String BLACK_TILE = "blackTile";
     public static final String BLUE_TILE = "blueTile";
@@ -34,11 +29,8 @@ public class Constants {
     public static final String WHITE_TILE = "whiteTile";
     public static final String YELLOW_TILE = "yellowTile";
     public static final String ONE_TILE = "oneTile";
-
     // Strings
     public static final String IMG_DIRECTORY = "/images/";
-
-
     // Gets Image from image folder
     public static BufferedImage getImage(String name) {
         try{
