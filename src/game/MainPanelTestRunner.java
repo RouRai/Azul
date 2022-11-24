82465042c46684fa1bf11409672488c34eb84820
NotHuayra <sam.qiao07@gmail.com>
2022-11-23 20:10:12 -0600

    board, logs jbutton done

---
+++ src/game/MainPanelTestRunner.java
@@ -0,0 +1,8 @@
+package game;
+import panels.MainPanel;
+import panels.MainTempFrame;
+public class MainPanelTestRunner {
+    public static void main(String[] args) {
+        MainTempFrame temp = new MainTempFrame("🤡");
+    }
+}
--- src/panels/MainPanel.java
+++ src/panels/MainPanel.java
@@ -6,6 +6,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
 
 import game.Constants;
 
@@ -13,28 +14,35 @@ public class MainPanel extends JPanel implements ActionListener{
 
     private CardLayout cl;
     private JButton returnStart;
+    private JButton Logs;
     private BufferedImage background;
+    private BufferedImage gameBoard;
 
     public MainPanel(CardLayout cl) {
         this.cl = cl;
         background = Constants.getImage("Background");
+        gameBoard = Constants.getImage("AzulBoard");
         setUpButtons();
+
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+        g.drawImage(gameBoard, 0, 0, (int)(getWidth() / 3), (int)(getWidth() / 3.5), null);
+        Logs.setBounds((int)(getWidth() / 1.3), (int)(getHeight() / 1.155), getWidth() / 8, getHeight() / 15);
     }
 
     private void setUpButtons() {
         // Instantiates JButtons
         returnStart = new JButton("Return to Start Screen");
-
+        Logs = new JButton("Logs");
         // Adds JButtons to the panels
         add(returnStart, BorderLayout.LINE_END);
-
+        super.add(Logs);
         // Adds action listeners to the JButtons
         returnStart.addActionListener(this);
+        Logs.addActionListener(this);
     }
 
     // Runs this method when something happens to one of the JButtons
